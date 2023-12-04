37c15d70afa1a3e8fd1d525c508851e5daae94be
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2023-12-04 16:25:29 +0100

    couleur aléatoire pour la boule

--- Assets/Scripts/Bullet_movement.cs
+++ Assets/Scripts/Bullet_movement.cs
@@ -14,6 +14,7 @@ public class Bullet_movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<Renderer>().material.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         double angle = UnityEngine.Random.Range(-1f, 1f) * Math.PI;
         movement = new Vector3((float) Math.Cos(angle), (float) Math.Sin(angle), 0);
     }
