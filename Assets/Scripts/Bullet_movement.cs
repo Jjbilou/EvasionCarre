e6f9dbda4cb513a61b84763d8a1369e4d1281291
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2023-12-04 16:09:12 +0100

    bouncing ball with random direction

---
+++ Assets/Scripts/Bullet_movement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Bullet_movement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        double angle = UnityEngine.Random.Range(-1f, 1f) * Math.PI;
+        movement = new Vector3((float) Math.Cos(angle), (float) Math.Sin(angle), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += movement * Time.deltaTime * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 7)
+        {
+            movement = new Vector3(movement.x, movement.y * -1, movement.z);
+        }
+
+        if (col.gameObject.layer == 8)
+        {
+            movement = new Vector3(movement.x * -1, movement.y, movement.z);
+        }
+    }
+}
