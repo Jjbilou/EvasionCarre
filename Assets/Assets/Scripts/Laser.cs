8e7b30ef1310792b0456d7e175591cbbc3b57f14
Nicolas <nico.dobosz@gmail.com>
2023-12-11 13:09:44 +0100

    Start Lv3 & fix fontion laser

--- Assets/Assets/Scripts/Laser.cs
+++ Assets/Assets/Scripts/Laser.cs
@@ -5,17 +5,12 @@ using DG.Tweening;
 
 public class Laser : MonoBehaviour
 {
-    public float laserWidth;
-    public float laserHeight;
-
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Renderer>().enabled = true;
         GetComponent<BoxCollider2D>().enabled = true;
         GetComponent<Animator>().enabled = true;
-
-        transform.localScale = new Vector3(laserWidth, laserHeight, 1f);
     }
 
     // Update is called once per frame
